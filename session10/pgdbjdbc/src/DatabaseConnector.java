e5a27f36f0fb50d17c6c28853c3e232ad0c64119
shkoohi <shkoohi@gmail.com>
2022-05-28 08:31:56 +0430

    add new

--- session10/pgdbjdbc/src/DatabaseConnector.java
+++ session10/pgdbjdbc/src/DatabaseConnector.java
@@ -1,7 +1,13 @@
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseConnector {
   private   Connection conn=null;
+
+    public DatabaseConnector() {
+      connect();
+    }
 
     public void connect(){
         String url = "jdbc:postgresql://localhost:5432/studentdb";
@@ -19,11 +25,11 @@ public class DatabaseConnector {
 
     }
 
-    public long insert(int id,String name,int age){
+    public long insert(String name,int age){
 long dbId=0;
 
-        String SQL = "INSERT INTO tbl_students(id,name,age) "
-                + "VALUES(?,?,?)";
+        String SQL = "INSERT INTO tbl_students(name,age) "
+                + "VALUES(?,?)";
 
 
 
@@ -31,9 +37,9 @@ long dbId=0;
             PreparedStatement pstmt = conn.prepareStatement(SQL,
                     Statement.RETURN_GENERATED_KEYS);
 
-            pstmt.setLong(1, id);
-            pstmt.setString(2, name);
-            pstmt.setInt(3, age);
+
+            pstmt.setString(1, name);
+            pstmt.setInt(2, age);
 
             int affectedRows = pstmt.executeUpdate();
             // check the affected rows
@@ -56,4 +62,156 @@ long dbId=0;
 
 
     }
+
+    public int updateName(long id, Student student) {
+        String SQL = "UPDATE tbl_students "
+                + "SET name = ? "
+                + "WHERE id = ?";
+
+        int affectedrows = 0;
+
+        try {
+
+             PreparedStatement pstmt = conn.prepareStatement(SQL) ;
+
+            pstmt.setString(1, student.getName());
+            pstmt.setLong(2,id);
+
+            affectedrows = pstmt.executeUpdate();
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return affectedrows;
+
+    }
+
+
+    public List<Student> filterByAge(int filteredAge) {
+
+        List<Student> students=new ArrayList<>();
+        String SQL = "SELECT  * from tbl_students "
+                + "WHERE age >= ?";
+
+
+
+        try {
+
+
+             PreparedStatement pstmt = conn.prepareStatement(SQL);
+
+            pstmt.setLong(1,filteredAge);
+
+            ResultSet resultSet = pstmt.executeQuery();
+
+            while(resultSet.next()){
+                //Display values
+                long id=    resultSet.getLong("id");
+                int age=     resultSet.getInt("age");
+                String name=     resultSet.getString("name");
+
+
+                Student student=new Student();
+                student.setAge(age);
+                student.setId(id);
+                student.setName(name);
+
+                students.add(student);
+
+            }
+
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return students;
+
+    }
+
+
+    public List<Student> getAll()  {
+
+        List<Student> students=new ArrayList<>();
+        String SQL = "SELECT  * from tbl_students ";
+
+
+        PreparedStatement pstmt = null;
+        try {
+            pstmt = conn.prepareStatement(SQL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        ResultSet resultSet = null;
+        try {
+            resultSet = pstmt.executeQuery();
+
+
+            while (resultSet.next()) {
+                //Display values
+                long id = resultSet.getLong("id");
+                int age = resultSet.getInt("age");
+                String name = resultSet.getString("name");
+
+
+                Student student = new Student();
+                student.setAge(age);
+                student.setId(id);
+                student.setName(name);
+
+                students.add(student);
+            }
+        }
+        catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+
+
+        return students;
+
+    }
+
+
+
+
+
+
+    public Student findById(long id ) throws SQLException {
+        String SQL = "SELECT  * from tbl_students "
+                + "WHERE id = ?";
+
+
+
+
+        PreparedStatement pstmt = conn.prepareStatement(SQL);
+
+        pstmt.setLong(1, id);
+
+        ResultSet resultSet = pstmt.executeQuery();
+        Student student = null;
+        while (resultSet.next()) {
+            //Display values
+            long ids = resultSet.getLong("id");
+            int age = resultSet.getInt("age");
+            String name = resultSet.getString("name");
+
+
+            student = new Student();
+            student.setAge(age);
+            student.setId(ids);
+            student.setName(name);
+            return student;
+
+        }
+        return null;
+
+
+    }
+
+
+
+
+
 }
--- session10/pgdbjdbc/src/Manager.java
+++ session10/pgdbjdbc/src/Manager.java
@@ -1,10 +1,19 @@
+import java.util.List;
+
 public class Manager {
     public static void main(String[] args) {
         DatabaseConnector ds=new DatabaseConnector();
-        ds.connect();
+
 
         System.out.println("--------");
-       long id= ds.insert(2,"Ali",28);
+    //   long id= ds.insert(2,"Ali",28);
+       long id= ds.insert("Ali"+System.currentTimeMillis(),28);
         System.out.println("id is="+id);
+
+      List<Student> studentList= ds.filterByAge(15);
+        List<Student> studens=  ds.getAll();
+
+
+
     }
 }
