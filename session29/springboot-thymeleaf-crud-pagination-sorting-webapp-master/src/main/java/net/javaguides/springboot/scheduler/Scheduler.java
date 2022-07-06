5bccaab2c94f615c7bf1017d9acf8023e4117d8c
Shabab Koohi <sh.k.na.1368@gmail.com>
2022-07-06 09:08:11 +0430

    Update Scheduler.java

--- session29/springboot-thymeleaf-crud-pagination-sorting-webapp-master/src/main/java/net/javaguides/springboot/scheduler/Scheduler.java
+++ session29/springboot-thymeleaf-crud-pagination-sorting-webapp-master/src/main/java/net/javaguides/springboot/scheduler/Scheduler.java
@@ -10,8 +10,17 @@ public class Scheduler {
         System.out.println(
                 "Fixed rate task - " + System.currentTimeMillis() / 1000);
     }
-//10:15 AM on the 15th day of every month.
-  //  @Scheduled(cron = "0 15 10 15 * M")
+//         "0 0 * * * *" = the top of every hour of every day.
+    //       "*/10 * * * * *" = every ten seconds.
+//            "0 0 8-10 * * *" = 8, 9 and 10 o'clock of every day.
+       //     "0 0 8,10 * * *" = 8 and 10 o'clock of every day.
+         //    "0 0/30 8-10 * * *" = 8:00, 8:30, 9:00, 9:30 and 10 o'clock every day.
+         //   "0 0 9-17 * * MON-FRI" = on the hour nine-to-five weekdays
+         //   "0 0 0 25 12 ?" = every Christmas Day at midnight
+
+
+
+    @Scheduled(cron = "*/10 * * * * *")
     public void scheduleTaskUsingCronExpression() {
 
         long now = System.currentTimeMillis() / 1000;
